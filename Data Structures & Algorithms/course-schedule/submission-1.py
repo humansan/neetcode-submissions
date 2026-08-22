b56b807cae0770fdfb5489cdfe12873f9454b9ae
humanSan <48076233+humansan@users.noreply.github.com>
2026-08-22 15:21:37 -0400

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        
+        course_reqs = defaultdict(list)
+
+        for a, b in prerequisites:
+            course_reqs[a].append(b)
+        
+        course_state = [0] * numCourses
+
+        def dfs(course):
+            if course_state[course] == 1:
+                return False
+            
+            if course_state[course] == 2:
+                return True
+            
+            course_state[course] = 1
+
+            for next_course in course_reqs[course]:
+                if not dfs(next_course): 
+                    return False
+            
+            course_state[course] = 2
+            return True
+        
+        for course in range(numCourses):
+            if not dfs(course):
+                return False
+        
+        return True
+
